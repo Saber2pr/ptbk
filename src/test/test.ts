3fe7b2e379898e123a040584cc90c4ba9aecd93e
saber2pr <1029985799@qq.com>
2020-11-19 09:23:07 +0800

    update

--- src/test/test.ts
+++ src/test/test.ts
@@ -10,10 +10,9 @@ const data = {
 
 const myPtbk = Ptbk.create(
   ([pri, pub]) => {
-    return 'yes' + pri + pub
+    return pri + pub
   },
   ptbk => {
-    ptbk = ptbk.slice(3)
     const m = ptbk.length / 2
     return [ptbk.slice(0, m), ptbk.slice(m)]
   }
@@ -22,24 +21,3 @@ const myPtbk = Ptbk.create(
 const ptbkRes = myPtbk.encode(data)
 console.log(ptbkRes)
 console.log(myPtbk.decode(ptbkRes))
-
-// 如果被破解，请更改以下加密方法
-export const ptbk = Ptbk.create(
-  ([pri, pub]) => {
-    const boundary_prefix = pri.slice(pri.length - 3)
-    return boundary_prefix + pri + pub
-  },
-  ptbk => {
-    ptbk = ptbk.slice(3)
-    const m = ptbk.length / 2
-    return [ptbk.slice(0, m), ptbk.slice(m)]
-  }
-)
-
-const ptbkRes2 = ptbk.encode({})
-console.log(ptbkRes2)
-console.log(ptbk.decode(ptbkRes2))
-console.log('isPtbk', ptbk.isPtbk(ptbkRes2))
-console.log('isPtbk', ptbk.isPtbk({}))
-console.log('isPtbk', ptbk.isPtbk(null))
-console.log('isPtbk', ptbk.isPtbk(undefined))
